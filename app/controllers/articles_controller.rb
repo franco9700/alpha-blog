85d6ec29aab43b27d8fe97959d1abbb82ea36f3d
franco9700 <francisco.iram97@gmail.com>
2020-03-26 17:47:29 -0700

    Add index article view working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
 	def show
 		#byebug detiene el servidor y arroja una consola de debug
 		@article = Article.find(params[:id])
