b2e70086c2289baf8308613ae97f386f80ee6b2d
franco9700 <francisco.iram97@gmail.com>
2020-03-26 18:20:41 -0700

    Add new view working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	
 	def index
 		@articles = Article.all
 	end
@@ -7,4 +8,17 @@ class ArticlesController < ApplicationController
 		#byebug detiene el servidor y arroja una consola de debug
 		@article = Article.find(params[:id])
 	end
+
+	def new
+	end
+
+	def create
+		#render plain: params[:article] #otra forma de debuguear
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		#render plain: @article.inspect
+		@article.save
+		#redirect_to article_path(@article)
+		redirect_to @article #version corta
+	end
+
 end
