b778936c1e2097d80be3dc161c8d445a3c94592f
franco9700 <francisco.iram97@gmail.com>
2020-03-26 18:38:01 -0700

    Add validation on New form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,15 +10,20 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
+		@article = Article.new
 	end
 
 	def create
 		#render plain: params[:article] #otra forma de debuguear
 		@article = Article.new(params.require(:article).permit(:title, :description))
 		#render plain: @article.inspect
-		@article.save
-		#redirect_to article_path(@article)
-		redirect_to @article #version corta
+		if @article.save
+			flash[:notice] = "Article was created succesfully" #ayuda a mostrar mensajes
+			#redirect_to article_path(@article)
+			redirect_to @article #version corta
+		else
+			render 'new' #regresa la pagina "new"
+		end
 	end
 
 end
