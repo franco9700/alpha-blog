48c470bf7cb15a7e1bdf459b25bc42101ac6a6cd
franco9700 <francisco.iram97@gmail.com>
2020-03-27 14:09:17 -0700

    Add refactored code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,13 @@
 class ArticlesController < ApplicationController
-	
+	before_action :set_article, only: [:show, :edit, :update, :destroy] #corre set_Article antes de cualquier cosa en los metodos especificados
+
+
 	def index
 		@articles = Article.all
 	end
 
 	def show
 		#byebug detiene el servidor y arroja una consola de debug
-		@article = Article.find(params[:id])
 	end
 
 	def new
@@ -14,12 +15,11 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	def create
 		#render plain: params[:article] #otra forma de debuguear
-		@article = Article.new(params.require(:article).permit(:title, :description)) #!!duda
+		@article = Article.new(article_params) #!!duda
 		#render plain: @article.inspect
 		if @article.save
 			flash[:notice] = "Article was created succesfully" #ayuda a mostrar mensajes
@@ -31,9 +31,8 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article = Article.find(params[:id])
 		#render plain: params[:article]
-		if @article.update(params.require(:article).permit(:title, :description))
+		if @article.update(article_params) #llamamos el metodo para refactorizar codigo
 			flash[:notice] = "Article was updated succesfully"
 			redirect_to @article
 		else
@@ -43,11 +42,18 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
-
 		if @article.destroy
 			redirect_to articles_path
 		end
+	end
+
+	private #Nada fuera de el controlador podra usar los siguientes metodos, no necesita end y siempre debe ir despues de todos los metodos
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :description)
 	end
 
 end
